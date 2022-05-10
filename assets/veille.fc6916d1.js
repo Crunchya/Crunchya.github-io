5cc723069b3fada6c92cf5762c0793079d9aee05
TheGDChatStats <thegdchatstats@gmail.com>
2022-05-10 20:57:16 +0200

    changed veilles page

---
+++ assets/veille.fc6916d1.js
@@ -0,0 +1,1 @@
+import{_ as o,D as t,r as n,o as s,c as a}from"./index.79080523.js";const c={components:{Default:t},mounted(){location.href.indexOf("#mesveilles")==-1&&(location.href=location.href+"#mesveilles")}};function r(l,f,i,p,_,m){const e=n("Default");return s(),a(e)}var d=o(c,[["render",r]]);export{d as default};
