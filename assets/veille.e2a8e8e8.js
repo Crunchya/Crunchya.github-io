21066b08a07989995d1bc4d3678040568f06dfa9
TheGDChatStats <thegdchatstats@gmail.com>
2022-05-10 21:06:04 +0200

    projects changed

--- assets/veille.e2a8e8e8.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,D as t,r as n,o as s,c as a}from"./index.8b8cd43b.js";const c={components:{Default:t},mounted(){location.href.indexOf("#mesveilles")==-1&&(location.href=location.href+"#mesveilles")}};function r(l,f,i,p,_,m){const e=n("Default");return s(),a(e)}var d=o(c,[["render",r]]);export{d as default};
