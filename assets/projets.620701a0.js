2151542627f849e88b77f21315892be992ddf18d
TheGDChatStats <thegdchatstats@gmail.com>
2022-05-11 17:18:00 +0200

    changes styleshhets import

---
+++ assets/projets.620701a0.js
@@ -0,0 +1,1 @@
+import{_ as e,D as t,r as n,o as r,c}from"./index.c45dad2c.js";const a={components:{Default:t},mounted(){location.href.indexOf("#projects")==-1&&(location.href=location.href+"#projects")}};function s(f,p,_,l,i,u){const o=n("Default");return r(),c(o)}var d=e(a,[["render",s]]);export{d as default};
