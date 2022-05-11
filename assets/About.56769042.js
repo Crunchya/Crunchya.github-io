d01546b1ce832466142f2c11622bbcbd20474bab
TheGDChatStats <thegdchatstats@gmail.com>
2022-05-11 16:06:09 +0200

    analytics + links

---
+++ assets/About.56769042.js
@@ -0,0 +1,1 @@
+import{_ as e,D as t,r as a,o as n,c}from"./index.4b697695.js";const r={components:{Default:t},mounted(){location.href.indexOf("#about")==-1&&(location.href=location.href+"#about")}};function s(f,p,u,_,l,i){const o=a("Default");return n(),c(o)}var d=e(r,[["render",s]]);export{d as default};
